2fffe363e664bdbe60e8d79366f60ab847718283
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-03-27 15:16:10 +0530

    Added resources to the public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
-// import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
 import Link from "next/link"
-// import Button from "./Button"
+import Image from "next/image";
 
 const Navbar = () => {
-return 
-  )
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
+      </Link>
+    </nav>
+)
 }
 
 export default Navbar
